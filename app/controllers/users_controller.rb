c207876504500c3e605fcff53f2edf227d22880a
Nathan Grey & David Adams <student@epicodus.com>
2014-04-16 14:39:46 -0700

    add user authentication with Devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "User created!"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  # def update
+  #   if @user.update(user_params)
+  #     flash[:notice] = "User updated!"
+  #     redirect_to user_path
+  #   else
+  #     render 'edit'
+  #   end
+  # end
+
+  # def destroy
+  #   @user.destroy
+  #   redirect_to users_path
+  # end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Airbnb::Application.routes.draw do
 
+  devise_for :users
   resources :properties
 
   root 'properties#index'
